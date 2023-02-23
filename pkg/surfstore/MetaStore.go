1abbe9c860fd3e54d7fca4836f041e27a95b63fd
Ajinkya Bokade <acbokade@gmail.com>
2023-02-23 14:08:34 -0800

    added RW locks MetaStore

--- pkg/surfstore/MetaStore.go
+++ pkg/surfstore/MetaStore.go
@@ -18,9 +18,9 @@ type MetaStore struct {
 // Returns mapping of files and its metadata (version, filename and hashlist)
 func (m *MetaStore) GetFileInfoMap(ctx context.Context, _ *emptypb.Empty) (*FileInfoMap, error) {
 	// Aqcuire read lock
-	// m.rwMutex.RLock()
+	m.rwMutex.RLock()
 	var fileInfoMap *FileInfoMap = &FileInfoMap{FileInfoMap: m.FileMetaMap}
-	// m.rwMutex.RUnlock()
+	m.rwMutex.RUnlock()
 	return fileInfoMap, nil
 }
 
@@ -29,35 +29,37 @@ func (m *MetaStore) UpdateFile(ctx context.Context, fileMetaData *FileMetaData) 
 	fileVersion := fileMetaData.Version
 	// Acquire read lock
 	// log.Println("server updateFile 1")
-	// m.rwMutex.RLock()
+	m.rwMutex.RLock()
 	_, exists := m.FileMetaMap[fileName]
-	// m.rwMutex.RUnlock()
+	m.rwMutex.RUnlock()
 	// log.Println("server updateFile 2")
 	if exists {
-		// m.rwMutex.RLock()
+		m.rwMutex.RLock()
 		curFileVersion := m.FileMetaMap[fileName].Version
-		// m.rwMutex.RLock()
+		m.rwMutex.RUnlock()
 		// log.Println("server updateFile 3")
 		// Replace the metadata only if the version is 1 greater than current file version
 		if fileVersion == 1+curFileVersion {
-			// m.rwMutex.Lock()
+			m.rwMutex.Lock()
 			m.FileMetaMap[fileName] = fileMetaData
-			// m.rwMutex.Unlock()
+			m.rwMutex.Unlock()
 		} else {
 			// Else send version -1 to the client
 			return &Version{Version: -1}, nil
 		}
 		// log.Println("server updateFile 4")
 	} else {
-		// m.rwMutex.Lock()
+		m.rwMutex.Lock()
 		m.FileMetaMap[fileName] = fileMetaData
-		// m.rwMutex.Unlock()
+		m.rwMutex.Unlock()
 	}
 	return &Version{Version: fileVersion}, nil
 }
 
 func (m *MetaStore) GetBlockStoreAddr(ctx context.Context, _ *emptypb.Empty) (*BlockStoreAddr, error) {
+	m.rwMutex.RLock()
 	var blockStoreAddr *BlockStoreAddr = &BlockStoreAddr{Addr: m.BlockStoreAddr}
+	m.rwMutex.RUnlock()
 	return blockStoreAddr, nil
 }
 
